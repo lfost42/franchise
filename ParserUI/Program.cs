269199801811618329f61648fd5574cb478f5b84
Lynda Foster <lfost42@wgu.edu>
2022-03-06 17:47:16 -0800

    refactored

--- ParserUI/Program.cs
+++ ParserUI/Program.cs
@@ -8,36 +8,15 @@ namespace ParserUI
 {
     class Program
     {
-        private static string csvFile;
+        private static string csvFile = "TacoBell-US-AL.csv";
         private static ParserControl control = new ParserControl();
 
         static void Main(string[] args)
         {
-            InitializeConfiguration();
-
-            csvFile = _config.GetValue<string>("TextFile");
-
-            //var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location.Substring(0, Assembly.GetEntryAssembly().Location.IndexOf("bin\\")));
-            //var filename = _config.GetValue<string>("TextFile");
-            //csvFile = path + filename;
-
-            //Console.WriteLine(csvFile);
-            //Console.ReadLine();
-
             var locations = control.ReadAllRecords(csvFile);
 
             control.GetFurthestLocations(locations);
             Console.ReadLine();
-        }
-
-        private static IConfiguration _config;
-        private static void InitializeConfiguration()
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json");
-
-            _config = builder.Build();
         }
 
     }
