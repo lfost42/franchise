d7cbb54a47ae7f1273961464072709a3bfd20dce
Lynda Foster <lfost42@wgu.edu>
2022-03-21 19:19:52 -0700

    FileModel

--- ParserLibrary/Models/FileModel.cs
+++ ParserLibrary/Models/FileModel.cs
@@ -1,8 +1,20 @@
 ﻿using System;
-namespace ParserLibrary.Models
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace SoftwarePlannerLibrary.Models
 {
     public class FileModel
     {
-
+        public IFormFile FormFile { get; set; }
+        public string FileName { get; set; }
+        public byte[] FileData { get; set; }
+        public string FileExtension { get; set; }
     }
+
 }
