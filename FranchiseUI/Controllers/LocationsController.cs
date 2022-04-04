79dfd5ec7828461b116ceeecc839637b47546c29
Lynda Foster <lfost42@wgu.edu>
2022-04-03 18:12:18 -0700

    locations view

---
+++ FranchiseUI/Controllers/LocationsController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using ParserLibrary;
+using ParserLibrary.Databases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FranchiseUI.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private readonly ILogger<LocationsController> _logger;
+
+        public LocationsController(ILogger<LocationsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ParserControl control = new ParserControl();
+            return View(control);
+        }
+
+        // GET: LocationsController
+        public ActionResult Index(ITrackable view, string csvFile)
+        {
+            if (ModelState.IsValid)
+            {
+                ParserControl control = new ParserControl();
+                csvFile = "Files/TacoBell-US-AL.csv";
+                ParserControl.GetAllLocations(csvFile);
+            }
+            return View(view);
+        }
+
+        // GET: LocationsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LocationsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LocationsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LocationsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LocationsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LocationsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LocationsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
